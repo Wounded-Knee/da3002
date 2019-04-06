dfb5ef20128226bec29ee8acce7eb7ea292479ee
JP Kramer <jpkramer707_github@mm.st>
2019-04-06 11:02:48 -0700

    Activated API server on port 3001 with endpoints specified but inactive. Added body-parser for POST. Added rudimentary cookie-based trustful user identification. Added support for POST /tests.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,92 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'da3002',
+  password: '3490vsjkllk52nm,a',
+  database: 'da3002'
+})
+connection.connect(err => { if (err) throw err; });
+
+router.get('/', lookupUser, undevelopedEndpoint);
+
+router.get('/users', lookupUser, undevelopedEndpoint);
+router.get('/users/me', lookupUser, undevelopedEndpoint);
+
+router.get('/tests', lookupUser, undevelopedEndpoint);
+
+router.post('/tests', lookupUser, (req, res, next) => {
+	connection.query(
+		'INSERT INTO Test (name, question) VALUES(?, ?)',
+		[req.body.name, req.body.question],
+		function(err, result) {
+			req.payload.error = err;
+			req.result = result;
+			res.json(req.payload);
+		}
+	);
 });
+
+router.put('/tests/:testId/answer/:answerId', lookupUser, undevelopedEndpoint);
+
+function undevelopedEndpoint(req, res, next) {
+	req.payload.text = "Go Away";
+	res.json(req.payload);
+}
+
+function lookupUser(req, res, next) {
+	const
+		setUserName = req.get('X-Name-User'),
+		reportedUserId = parseInt(req.cookies.user);
+	req.payload = {
+		meta: {}
+	};
+	req.user = {};
+
+	const getUserById = (userId) => {
+		return new Promise((resolve, reject) => {
+			connection.query(
+				'SELECT * FROM User WHERE id=?',
+				[ userId ],
+				(err, result) => {
+					req.payload.meta.foundUser = userId;
+					req.user = result;
+					resolve(result);
+				}
+			);		
+		})
+	}
+
+	if (reportedUserId) {
+		getUserById(reportedUserId).then((user) => {
+			next();
+		})
+	} else {
+		if (setUserName) {
+			req.payload.meta.namedUser = setUserName;
+		}
+		const newUserName = setUserName || 'Anonymous';
+		new Promise((resolve, reject) => {
+			if (!reportedUserId) {
+				connection.query(
+					'INSERT INTO User (name) VALUES(?)',
+					[ newUserName ],
+					(err, result) => {
+						req.payload.meta.createdUser = result.insertId;
+						res.cookie('user', result.insertId, {maxAge: 360000});
+						resolve(result.insertId);
+					}
+				)
+			} else {
+				resolve(reportedUserId);
+			}
+		}).then((userId) => {
+			getUserById(userId).then(user => next());
+		})
+		//req.payload.meta.anonymousUser = true;
+	}
+}
 
 module.exports = router;
